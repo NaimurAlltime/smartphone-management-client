4d2252e68268dafda1edf53b364fc28d0c8367d9
NaimurAlltime <naimur2231@gmail.com>
2024-01-26 16:30:54 +0600

    Layout Component With Ant Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import MainLayout from "./components/layout/MainLayout";
+
 function App() {
-  return (
-    <>
-      <h2>smartphone management system</h2>
-    </>
-  );
+  return <MainLayout />;
 }
 
 export default App;
