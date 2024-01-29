e217e69896548c8211c577829ea9d17e5c22467e
NaimurAlltime <naimur2231@gmail.com>
2024-01-29 12:48:25 +0600

    all smartphone table design done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import MainLayout from "./components/layout/MainLayout";
 import ProtectedRoute from "./components/layout/ProtectedRoute";
+import { Datepicker, Input, initTE } from "tw-elements";
+initTE({ Datepicker, Input }, { allowReinits: true });
 
 function App() {
   return (
--- src/pages/SmartphoneManagement/GetAllSmartphone.tsx
+++ src/pages/SmartphoneManagement/GetAllSmartphone.tsx
@@ -1,12 +1,86 @@
 import { useGetAllSmartphoneQuery } from "../../redux/features/smartphone/smartphoneApi";
+import { MdOutlineAddShoppingCart } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+import { RiDeleteBinLine } from "react-icons/ri";
 
 function GetAllSmartphone() {
   const { data: smartphones } = useGetAllSmartphoneQuery(undefined);
   console.log(smartphones?.data);
   return (
-    <>
-      <h2>all smartphone</h2>
-    </>
+    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="py-6 px-4 text-center md:px-6 xl:px-7.5">
+        <h4 className="text-xl font-semibold text-black dark:text-white">
+          All Smartphone
+        </h4>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full text-left text-sm font-light">
+                <thead className="border-b font-medium dark:border-neutral-500">
+                  <tr>
+                    <th scope="col" className="px-6 py-4">
+                      #
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Smartphone Name
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Category
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Price
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Quantity
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Brand
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Model
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Storage
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="border-b dark:border-neutral-500">
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">
+                      1
+                    </td>
+                    <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                    <td className="whitespace-nowrap px-6 py-4">Otto</td>
+                    <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                    <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                    <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                    <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                    <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                    <td className="whitespace-nowrap px-6 py-4 felx justify-center items-center">
+                      <button>
+                        <MdOutlineAddShoppingCart className="text-2xl" />
+                      </button>
+                      <button className="mx-3">
+                        <FaRegEdit className="text-2xl" />
+                      </button>
+                      <button>
+                        <RiDeleteBinLine className="text-2xl" />
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
